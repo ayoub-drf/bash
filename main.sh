f136f0caaba64607e2fc26d6776fbce84ca00ccf
Ayoub <ayoubhost0@gmail.com>
2024-12-23 20:06:17 +0100

    bashhhhhhh

--- main.sh
+++ main.sh
@@ -1,1 +1,25 @@
-echo "Hello world"
+#!/bin/bash
+
+# echo "Enter a file path: "
+# read file_path
+
+# if [[ -e $file_path ]]; then
+#     echo "File exists on $file_path"
+#     exit 0
+# else 
+#     echo "File does not exists on $file_path"
+#     exit 1
+# fi
+
+# echo "Updating the system..."
+# sudo apt update
+
+#!/bin/bash
+
+# Loops in bash
+
+# for i in *;
+# do
+#     echo $i >> files.txt
+# done
+
